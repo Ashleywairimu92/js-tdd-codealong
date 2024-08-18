305f30e2770dea52306c0f9913d4c19c197619a3
Ashleywairimu92 <ashleywairimu18@gamil.com>
2024-08-19 01:44:15 +0300

    updates

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,15 @@
 // Your code here
+import React from 'react';
+import { currentAgeForBirthYear } from '../utils';
+
+const AgeDisplay = ({ birthYear }) => {
+  const age = currentAgeForBirthYear(birthYear);
+
+  return (
+    <div>
+      <p>Your age is: {age}</p>
+    </div>
+  );
+};
+
+export default AgeDisplay;
